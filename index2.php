3e098fb850a4d0b6da54a4ac55ad3aacdd0d8e37
Alejandro Moreno <morenorubio98@users.noreply.github.com>
2019-04-11 15:39:54 -0600

    Se agrego un archivo index

--- index2.php
+++ index2.php
@@ -5,9 +5,4 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-echo "Hola mundo";
-/*Este es mi primer hola mundo php Github*/
-echo 'hola mundo 1';
-echo 'hola mundo 2';
-echo 'hola mundo 3';
+echo 'hola mundo 4';
